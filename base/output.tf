1909dff15f327918bb2b98adc726836215824b0b
Adriano Costa <adriano.dmcosta@gmail.com>
2025-04-07 20:46:40 -0300

    mais uma tentativa

--- base/output.tf
+++ base/output.tf
@@ -1,4 +1,4 @@
-# output "rds_videos_instance_address" {
-#   value = aws_db_instance.snacktech_db_pedidos.address
-#   description = "Endereço da instancia RDS do banco de videos"
-# }
+output "rds_videos_instance_address" {
+  value = aws_db_instance.snacktech_db_pedidos.address
+  description = "Endereço da instancia RDS do banco de videos"
+}
