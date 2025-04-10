c11b2fdfa18c52596ff4d7fa7207f24315a0958f
Adriano Costa <adriano.dmcosta@gmail.com>
2025-04-10 12:22:46 -0300

    restringir acesso eks apenas a port 8080

--- base/eks-sg.tf
+++ base/eks-sg.tf
@@ -2,28 +2,12 @@ resource "aws_security_group" "sg" {
   name        = "SG-${var.projectName}"
   description = "Este grupo e usado no snap-vid"
   vpc_id      = data.aws_vpc.vpc.id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  
   ingress {
     description = "HTTP"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "ALL_IN"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
