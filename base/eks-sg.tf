4199fea3e1b9d219dca2f3e32f025107cc8b050f
Adriano Costa <adriano.dmcosta@gmail.com>
2025-04-10 07:34:51 -0300

    testar liberação de trafego de entrada eks

--- base/eks-sg.tf
+++ base/eks-sg.tf
@@ -11,6 +11,22 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ALL_IN"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "All"
     from_port   = 0
