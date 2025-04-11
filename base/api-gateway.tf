53934cb792bff0c77cc3340e626e41c9f4e4bac0
Adriano Costa <adriano.dmcosta@gmail.com>
2025-04-11 11:09:25 -0300

    retirar integração redundante da lambda

--- base/api-gateway.tf
+++ base/api-gateway.tf
@@ -20,6 +20,28 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_authorizer" "lambda_auth" {
+  depends_on = [ aws_api_gateway_rest_api.main, aws_lambda_function.auth_lambda ]
+  name          = "LambdaAuth"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  authorizer_uri = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.auth_lambda.arn}/invocations"
+  type          = "TOKEN"  # Mantenha como TOKEN para JWT
+  identity_source = "method.request.header.Authorization"
+  
+  # Adicione essas linhas:
+  authorizer_credentials = data.aws_iam_role.labrole.arn
+  authorizer_result_ttl_in_seconds = 300
+}
+
+resource "aws_lambda_permission" "allow_apigateway" {
+  depends_on = [ aws_lambda_function.auth_lambda, aws_api_gateway_rest_api.main ]
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.auth_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+}
+
 resource "aws_api_gateway_method" "proxy" {
   depends_on = [ aws_api_gateway_rest_api.main, aws_api_gateway_resource.proxy, aws_api_gateway_authorizer.lambda_auth ]
   rest_api_id   = aws_api_gateway_rest_api.main.id
@@ -56,39 +78,6 @@ resource "aws_api_gateway_integration" "proxy" {
   connection_id   = aws_api_gateway_vpc_link.main.id
 }
 
-resource "aws_api_gateway_authorizer" "lambda_auth" {
-  depends_on = [ aws_api_gateway_rest_api.main, aws_lambda_function.auth_lambda ]
-  name          = "LambdaAuth"
-  rest_api_id   = aws_api_gateway_rest_api.main.id
-  authorizer_uri = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.auth_lambda.arn}/invocations"
-  type          = "TOKEN"  # Mantenha como TOKEN para JWT
-  identity_source = "method.request.header.Authorization"
-  
-  # Adicione essas linhas:
-  authorizer_credentials = data.aws_iam_role.labrole.arn
-  authorizer_result_ttl_in_seconds = 300
-}
-
-resource "aws_lambda_permission" "allow_apigateway" {
-  depends_on = [ aws_lambda_function.auth_lambda, aws_api_gateway_rest_api.main ]
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.auth_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_integration" "lambda_integration" {
-  depends_on = [ aws_api_gateway_rest_api.main, aws_api_gateway_resource.proxy, aws_api_gateway_method.proxy, aws_lambda_function.auth_lambda ]
-  rest_api_id             = aws_api_gateway_rest_api.main.id
-  resource_id             = aws_api_gateway_resource.proxy.id
-  http_method             = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.auth_lambda.invoke_arn
-}
-
-# Deployment e Stage (sem alterações)
 resource "aws_api_gateway_stage" "dev_stage" {
   depends_on = [ aws_api_gateway_rest_api.main, aws_api_gateway_deployment.api_deployment ]
   rest_api_id   = aws_api_gateway_rest_api.main.id
